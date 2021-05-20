0446293c70761c4f5844d2b179fa6b12fdfcfeb4
mistico0121 <mrschudeck@uc.cl>
2021-05-20 10:20:35 -0400

    barebones for new app

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const HandlePressEvent = () => console.log("Text pressed")
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image 
+        style={{
+          height: 100,
+          width: 100
+        }}
+        source={require('./assets/cloudnet-logo.png')}
+      />
+      <Text onPress={HandlePressEvent}>Press me maggot!</Text>
+      <Button 
+        color="red"
+        title="Continue as Client" 
+        onPress={()=> console.log("Continue as client")}>
+      </Button>
+      <Button 
+        title="Login as Worker" 
+        onPress={()=> console.log("Logueado como trabajador, mandar a menú login")}>
+      </Button>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c8e6f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
